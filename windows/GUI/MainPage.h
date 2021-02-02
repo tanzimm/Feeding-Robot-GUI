8bd689e17acf8f6a1b18d832e93f1151a4344679
tanzimm <tanzim96@hotmail.com>
2021-02-02 17:37:13 -0500

    first

---
+++ windows/GUI/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::GUI::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::GUI::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
